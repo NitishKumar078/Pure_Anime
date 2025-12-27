98d667eb288c046feb845c034501400869fb9671
nitish kumar <nishkumar562@gmail.com>
2025-12-27 22:00:34 +0530

    Cleanup

--- src/react/Component/App.tsx
+++ src/react/Component/App.tsx
@@ -1,7 +1,6 @@
 import Button from "./ui/Button/Button";
 import Mode from "./ui/mode/mode";
 import "./App.css";
-import Freatures from "./Features/Freatures";
 import AnimeList from "./AnimeList/AnimeList";
 
 const App = () => {
@@ -9,14 +8,7 @@ const App = () => {
     <div className="App">
       <h1>Pure Anime</h1>
       <Button />
-      {/* <div id="mode" title="Applies the options feature">
-        <label>
-          <span>Strict Mode :</span>
-        </label>
-        <Mode />
-      </div> */}
       <AnimeList />
-      {/* <Freatures /> */}
     </div>
   );
 };
